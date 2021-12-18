3ed3fc8146fcdd5b5c41d3c51f8e78d363c73352
Arpitha <justforinfo@ymail.com>
2021-12-18 10:43:03 +0000

    Second submission

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -9,14 +9,13 @@ dockerpath=justinfo/devops_project4
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run latest --image=$dockerpath --port=80
+/home/ec2-user/environment/DevOps_Microservices/project-ml-microservice-kubernetes/minikube kubectl -- run latest --image=$dockerpath --port=80
 
 
 # Step 3:
 # List kubernetes pods
-kubectl get pods
+/home/ec2-user/environment/DevOps_Microservices/project-ml-microservice-kubernetes/minikube kubectl -- get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward latest 8000:80
-
+/home/ec2-user/environment/DevOps_Microservices/project-ml-microservice-kubernetes/minikube kubectl -- port-forward latest 8000:80
